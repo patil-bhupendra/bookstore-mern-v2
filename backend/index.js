c90296f5a5756f9758f42f8396312b958fdb5d69
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-04-24 09:14:37 +0530

    feat: initialize backend with MongoDB connection and feature-based folder structure

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.use('/', (req, res) => {
+    res.send('Book store server is running!')
+  })
+}
+
+main().then(() => console.log("Mongodb connect sucessfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
