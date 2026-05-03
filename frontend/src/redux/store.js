09692c02a647969361b1fb3aa24b4145d533cc4e
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-05-03 09:34:33 +0530

    fix: resolve API URL issues and enable category-based book filtering

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from "../redux/features/cart/cartSlice"
+import booksApi from './features/cart/booksApi'
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer
+    cart: cartReducer,
+    [booksApi.reducerPath]: booksApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
 })
