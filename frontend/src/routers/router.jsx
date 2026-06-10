d5585f0045cdd45f6d352008f4b16d5b89c3e1e4
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-06-10 16:26:59 +0530

    feat: add book update functionality with pre-filled form data

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -14,6 +14,7 @@ import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
 import AddBook from "../pages/dashboard/addBook/AddBook";
+import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
 
 const router = createBrowserRouter([
   {
@@ -94,7 +95,7 @@ const router = createBrowserRouter([
         path: "edit-book/:id",
         element: (
           <AdminRoute>
-            <div>Edit Book</div>
+            <UpdateBook/>
           </AdminRoute>
         ),
       },
