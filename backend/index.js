9153e8612aaf23615421f05af6cd11d996355c99
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-06-10 17:27:04 +0530

    fix: update production API URL and CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://bookstore-mern-v2.vercel.app"],
     credentials: true,
   }),
 );
@@ -18,14 +18,13 @@ app.use(
 // routes
 const bookRoutes = require("./src/books/book.route");
 const orderRoutes = require("./src/orders/order.route");
-const userRoutes = require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
 app.use("/api/books", bookRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
-
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Book store server is running!");
--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 const getBaseUrl = () => {
-    return "http://localhost:5000"
+    return "https://bookstore-mern-v2.onrender.com"
 }
 
 export default getBaseUrl;
