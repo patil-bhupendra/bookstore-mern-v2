50523e216d41964ed2efcf3f55048fd6623c4511
Bhupendra Patil <bhupendrarajput1232@gmail.com>
2026-06-10 16:07:34 +0530

    feat: implement Add Book functionality

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -13,6 +13,7 @@ import AdminLogin from "../components/AdminLogin";
 import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ManageBooks from "../pages/dashboard/manageBooks/ManageBooks";
+import AddBook from "../pages/dashboard/addBook/AddBook";
 
 const router = createBrowserRouter([
   {
@@ -85,7 +86,7 @@ const router = createBrowserRouter([
         path: "add-new-book",
         element: (
           <AdminRoute>
-            <div>Add New Book</div>
+            <AddBook/>
           </AdminRoute>
         ),
       },
